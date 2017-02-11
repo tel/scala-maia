5248d37c99b720c1bf89e25370d2e16469d1f1eb
Joseph Abrahamson <me@jspha.com>
2017-02-11 02:04:37 -0500

    Add some notes about Handlers and Servers

--- src/main/scala-2.12/com/jspha/maia/Handler.scala
+++ src/main/scala-2.12/com/jspha/maia/Handler.scala
@@ -6,6 +6,15 @@ package com.jspha.maia
 
 import scala.language.higherKinds
 
+/*
+
+  Handlers are the generic representation of Request ~> Response transmission.
+  In particular, on the server side a Handler will construct actual Response
+  values. On the client side it's more likely to be a caching/forwarding
+  layer that corresponds with the aforementioned Server.
+
+ */
+
 trait Handler[M[_], Api[_ <: Mode]] {
   def run(request: Request[Api]): M[Response[Api]]
 }
--- src/main/scala-2.12/com/jspha/maia/Server.scala
+++ src/main/scala-2.12/com/jspha/maia/Server.scala
@@ -8,3 +8,14 @@ import scala.language.higherKinds
 
 abstract class Server[M[_], Api[_ <: Mode]](interpreter: Interpreter[M, Api])
   extends Handler[M, Api]
+
+/*
+
+  TODO: Implement the Server.
+
+  For each "requested" field in the Api, we pass the request through the
+  given Interpreter, flattening each field request's M layer monadically. At
+  the leaves, we'll obtain result values which we can reassemble into a
+  response.
+
+ */
